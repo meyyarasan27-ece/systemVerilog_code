b14a08ecfeaf93b9f004670179d19bbffe62d4e9
meyyarasan27-ece <meyyarasan27@gmail.com>
2026-06-29 18:21:14 +0530

    Create driver.sv

---
+++ driver.sv
@@ -0,0 +1,25 @@
+class driver;
+  virtual intf vif ;
+  mailbox gen2drv ;
+  
+  
+  function new(virtual intf vif , mailbox gen2drv) ;
+    this.vif = vif;
+    this.gen2drv = gen2drv ;
+  endfunction
+  
+  task main();
+    repeat(8)
+      begin
+        transaction trans ;
+        gen2drv.get(trans) ;
+        vif.a <= trans.a ;
+        vif.b <= trans.b ;
+        vif.c <= trans.c ;
+        
+        #1 ;
+        trans.display(" driver class signal") ;
+      end
+      
+  endtask
+endclass
