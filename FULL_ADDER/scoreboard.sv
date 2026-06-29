bbf1475446d1f84c4a995f246a21692f3e71b9ca
meyyarasan27-ece <meyyarasan27@gmail.com>
2026-06-29 18:28:29 +0530

    Create scoreboard.sv

---
+++ FULL_ADDER/scoreboard.sv
@@ -0,0 +1,26 @@
+class scoreboard ;
+  mailbox mon2scb ;
+  
+  function new(mailbox mon2scb) ;
+    this.mon2scb = mon2scb ;
+  endfunction
+  
+  task main() ;
+    transaction trans ;
+    repeat(8)
+      begin
+        mon2scb.get(trans);
+        trans.display("scoreboard signals") ;
+        
+        if(((trans.a ^ trans.b ^ trans.c ) == trans.sum) &&
+           ((( trans.a & trans.b) | 
+             ( trans.b & trans.c) |
+             ( trans.c & trans.a)) == trans.carry))
+          $display("---PASS---");
+        else 
+          $display("---FAIL---");
+        
+       $display("---TRANSACTION DONE---");         
+      end
+  endtask
+endclass
