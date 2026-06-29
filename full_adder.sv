778169a463b0985f58b80c74764e822616c4342d
meyyarasan27-ece <meyyarasan27@gmail.com>
2026-06-29 18:17:38 +0530

    Update full_adder.sv

--- full_adder.sv
+++ full_adder.sv
@@ -1,1 +1,10 @@
-
+module full_adder( input logic a ,
+                 input logic b ,
+                 input logic c ,
+                 output logic sum ,
+                 output logic carry) ;
+  always_comb begin
+    sum = a ^ b^ c ;
+    carry = ( a & b ) | ( b & c )  | ( a & c ) ;
+  end
+endmodule
